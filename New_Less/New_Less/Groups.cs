74fcabcb311f247a002884f1ca6133e33fc659fa
RahimovaBanovsha <rahimovabanovsha@gmail.com>
2025-06-15 15:11:03 +0400

    Delete New_Less directory

--- New_Less/New_Less/Groups.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace New_less;
-public class Groups: IEnumerable
-{
-    public Guid Id { get; set; } = Guid.NewGuid();
-    public string? Name { get; set; }
-    public Student[]? Students { get; set; }
-
-}
